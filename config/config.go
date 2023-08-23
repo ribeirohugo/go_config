3eba985ad46b13dad39d6d90d1abe8554c0dbdc1
Hugo Ribeiro <ribeirohugo@outlook.pt>
2023-08-23 22:10:22 +0100

    chore: separated `config` model from load files

--- config/config.go
+++ config/config.go
@@ -17,49 +17,6 @@ const (
 	defaultJaegerHost = "http://localhost:14268/api/traces"
 )
 
-// Database holds database connection configurations.
-type Database struct {
-	Host           string `toml:"host"`
-	Port           int    `toml:"port"`
-	User           string `toml:"user"`
-	Password       string `toml:"password"`
-	Db             string `toml:"database"`
-	MigrationsPath string `toml:"migrations_path"`
-}
-
-// Server holds server host and port configurations.
-type Server struct {
-	Host           string   `toml:"host"`
-	Port           int      `toml:"port"`
-	AllowedOrigins []string `toml:"allowed_origins"`
-}
-
-type Token struct {
-	MaxAge int    `toml:"max_age"`
-	Secret string `toml:"secret"`
-}
-
-// Tracer holds jaeger tracer config attributes
-type Tracer struct {
-	Enabled    bool   `toml:"enabled"`
-	JaegerHost string `toml:"jaeger_host"`
-}
-
-// Config holds configurations data and methods.
-type Config struct {
-	Server Server `toml:"server"`
-	Token  Token  `toml:"token"`
-
-	MongoDb  Database `toml:"mongodb"`
-	MySql    Database `toml:"mysql"`
-	Postgres Database `toml:"postgres"`
-
-	Tracer Tracer `toml:"tracer"`
-
-	Environment string `toml:"environment"`
-	Service     string `toml:"service"`
-}
-
 // Load loads configurations from a given toml file path.
 func Load(filePath string) (Config, error) {
 	file, err := os.Open(filePath)
