3bea4a07004a11003ac3c86be147c89953174a1d
Hugo Ribeiro <56323467+ribeirohugo@users.noreply.github.com>
2023-08-24 23:25:06 +0100

    chore: remove load methods redundancy (#7)
    * Encapsulate `LoadContent` methods into `Load` to avoid redundancy.

--- config/yaml/config.go
+++ config/yaml/config.go
@@ -22,30 +22,7 @@ func Load(filePath string) (config.Config, error) {
 	}
 	_ = file.Close()
 
-	cfg := config.Config{
-		MySql: config.Database{
-			MigrationsPath: config.DefaultMigrationsMysql,
-		},
-		MongoDb: config.Database{
-			MigrationsPath: config.DefaultMigrationsMongo,
-		},
-		Postgres: config.Database{
-			MigrationsPath: config.DefaultMigrationsPostgres,
-		},
-		Token: config.Token{
-			MaxAge: config.DefaultSessionMaxAge,
-		},
-		Tracer: config.Tracer{
-			JaegerHost: config.DefaultJaegerHost,
-		},
-	}
-
-	err = yaml.Unmarshal(bytes, &cfg)
-	if err != nil {
-		return config.Config{}, err
-	}
-
-	return cfg, nil
+	return LoadContent(bytes)
 }
 
 // LoadContent loads configurations from a given yaml bytes content.
