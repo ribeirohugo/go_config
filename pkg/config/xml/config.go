e542acc2ced4be9d6b9c6af7e8570b9bc576fe59
Hugo Ribeiro <ribeirohugo@outlook.pt>
2025-04-09 17:26:53 +0100

    chore: update `XML` config loader

--- pkg/config/xml/config.go
+++ pkg/config/xml/config.go
@@ -9,15 +9,15 @@ import (
 )
 
 // Load loads configurations from a given XML file path.
-func Load(filePath string) (config.Config, error) {
+func Load(filePath string) (config.XML, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
-		return config.Config{}, err
+		return config.XML{}, err
 	}
 
 	bytes, err := io.ReadAll(file)
 	if err != nil {
-		return config.Config{}, err
+		return config.XML{}, err
 	}
 	_ = file.Close()
 
@@ -25,37 +25,39 @@ func Load(filePath string) (config.Config, error) {
 }
 
 // LoadContent loads configurations from a given xml bytes content.
-func LoadContent(content []byte) (config.Config, error) {
-	cfg := config.Config{
-		MySql: config.Database{
-			Port:           config.DefaultMySQLPort,
-			MigrationsPath: config.DefaultMigrationsMysql,
-		},
-		MongoDb: config.Database{
-			Port:           config.DefaultMongoPort,
-			MigrationsPath: config.DefaultMigrationsMongo,
-		},
-		Postgres: config.Database{
-			Port:           config.DefaultPostgresPort,
-			MigrationsPath: config.DefaultMigrationsPostgres,
-		},
-		Loki: config.ExternalService{
-			Host: config.DefaultLokiHost,
-		},
-		Tempo: config.ExternalService{
-			Host: config.DefaultTempoHost,
-		},
-		Token: config.Token{
-			MaxAge: config.DefaultSessionMaxAge,
-		},
-		Jaeger: config.ExternalService{
-			Host: config.DefaultJaegerHost,
+func LoadContent(content []byte) (config.XML, error) {
+	cfg := config.XML{
+		Config: config.Config{
+			MySql: config.Database{
+				Port:           config.DefaultMySQLPort,
+				MigrationsPath: config.DefaultMigrationsMysql,
+			},
+			MongoDb: config.Database{
+				Port:           config.DefaultMongoPort,
+				MigrationsPath: config.DefaultMigrationsMongo,
+			},
+			Postgres: config.Database{
+				Port:           config.DefaultPostgresPort,
+				MigrationsPath: config.DefaultMigrationsPostgres,
+			},
+			Loki: config.ExternalService{
+				Host: config.DefaultLokiHost,
+			},
+			Tempo: config.ExternalService{
+				Host: config.DefaultTempoHost,
+			},
+			Token: config.Token{
+				MaxAge: config.DefaultSessionMaxAge,
+			},
+			Jaeger: config.ExternalService{
+				Host: config.DefaultJaegerHost,
+			},
 		},
 	}
 
 	err := xml.Unmarshal(content, &cfg)
 	if err != nil {
-		return config.Config{}, err
+		return config.XML{}, err
 	}
 
 	return cfg, nil
